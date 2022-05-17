6c67ab66f1c34cd8da85ab276801457dfcd7bf51
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2022-05-17 15:54:23 +0200

    refactored test

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 import pytest
 
-from pyroll import solve
+import pyroll.core
+import pyroll.ui
 from pyroll.ui.reporter import Reporter
 
 THIS_DIR = Path(__file__).parent
@@ -11,15 +12,15 @@ THIS_DIR = Path(__file__).parent
 
 def test_solve(tmp_path: Path, caplog):
     import pyroll.ui.cli.res.input_trio as input_py
-    import pyroll_roux_spreading
+    import pyroll.roux_spreading
 
-    caplog.set_level(logging.DEBUG, logger="pyroll_roux_spreading")
+    caplog.set_level(logging.DEBUG, logger="roux_spreading")
 
     sequence = input_py.sequence
 
-    solve(sequence, input_py.in_profile)
+    pyroll.core.solve(sequence, input_py.in_profile)
 
-    report = Reporter()
+    report = pyroll.ui.Reporter().render(input_trio.sequence)
 
     rendered = report.render(sequence)
     print()
