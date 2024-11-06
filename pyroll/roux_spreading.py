9cad08a9bc04334bf083c0c664127a29f58d3f45
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2024-11-06 08:38:27 +0100

    updated according to new pyroll-core, added test, bump. version.

--- pyroll/roux_spreading.py
+++ pyroll/roux_spreading.py
@@ -1,20 +1,20 @@
 import numpy as np
-from pyroll.core import RollPass, root_hooks, Unit, ThreeRollPass
+from pyroll.core import BaseRollPass, root_hooks, Unit, ThreeRollPass
 from pyroll.core.hooks import Hook
 
-VERSION = "2.0"
+VERSION = "2.0.1"
 
 root_hooks.add(Unit.OutProfile.width)
 
-RollPass.first_roux_parameter = Hook[float]()
+BaseRollPass.first_roux_parameter = Hook[float]()
 """First parameter a of Roux's spread equation."""
 
-RollPass.second_roux_parameter = Hook[float]()
+BaseRollPass.second_roux_parameter = Hook[float]()
 """Second parameter b of Roux's spread equation."""
 
 
-@RollPass.first_roux_parameter
-def first_roux_parameter(self: RollPass):
+@BaseRollPass.first_roux_parameter
+def first_roux_parameter(self: BaseRollPass):
     equivalent_height_change = self.in_profile.equivalent_height - self.out_profile.equivalent_height
 
     if np.isclose(equivalent_height_change, 0):
@@ -25,15 +25,15 @@ def first_roux_parameter(self: RollPass):
         self.in_profile.equivalent_height / equivalent_height_change - 1)
 
 
-@RollPass.second_roux_parameter
-def second_roux_parameter(self: RollPass):
+@BaseRollPass.second_roux_parameter
+def second_roux_parameter(self: BaseRollPass):
     return (self.in_profile.equivalent_width / self.in_profile.equivalent_height - 1) * (
             self.in_profile.equivalent_width / self.in_profile.equivalent_width) ** (
             2 / 3)
 
 
-@RollPass.spread
-def spread(self: RollPass):
+@BaseRollPass.spread
+def spread(self: BaseRollPass):
     equivalent_height_change = self.in_profile.equivalent_height - self.out_profile.equivalent_height
 
     second_factor = 1 / (
@@ -46,18 +46,8 @@ def spread(self: RollPass):
     return 1 + (equivalent_height_change * second_factor * third_factor) / self.in_profile.equivalent_width
 
 
-@RollPass.OutProfile.width
-def width(self: RollPass.OutProfile):
-    rp = self.roll_pass
-
-    if not self.has_set_or_cached("width"):
-        return None
-
-    return rp.spread * rp.in_profile.width
-
-
-@ThreeRollPass.OutProfile.width
-def width(self: RollPass.OutProfile):
+@BaseRollPass.OutProfile.width
+def width(self: BaseRollPass.OutProfile):
     rp = self.roll_pass
 
     if not self.has_set_or_cached("width"):
